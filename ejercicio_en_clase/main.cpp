7a8b9dc5ab7a72bbca191aa8d776a23e08ab6e63
JDRV17 <123598113+JDRV17@users.noreply.github.com>
2023-05-07 20:16:22 -0500

    Update main.cpp

--- ejercicio_en_clase/main.cpp
+++ ejercicio_en_clase/main.cpp
@@ -6,11 +6,18 @@ int main()
 {
     int i = 1;
     colaPrioridad c;
+    c.enqueue(0);
     c.enqueue(59);
     c.enqueue(10);
     c.enqueue(46);
     c.enqueue(15);
     c.enqueue(2);
+    c.enqueue(12);
+    c.enqueue(5);
+    c.enqueue(1);
+    c.enqueue(28);
+    c.enqueue(85);
+    c.enqueue(26);
     c.enqueue(12);
     cout << "Los números de la cola de menor a mayor son: \n";
     while (!c.vaciaCola())
