47f998f6db09160fb760f23f5ac8f6bc02ddd6d6
JDRV17 <123598113+JDRV17@users.noreply.github.com>
2023-05-30 03:53:01 -0500

    Update main.cpp

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -3,5 +3,22 @@
 
 int main()
 {
-
+    string hello = "1", hello2 = "2";
+    BigInteger x = hello;
+    BigInteger y = hello2;
+    int z = -2;
+    //x.add(y);
+    //x.substract(y);
+    //x.product(y);
+    //x.quotient(y);
+    //x.remainder(y);
+    //x.pow(z);
+    /*if(x >= y)
+    {
+        cout << "hello";
+    }*/
+    if(x < y)
+    {
+        cout << "holi";
+    }
 }
